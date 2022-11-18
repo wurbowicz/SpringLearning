d3024d5658e2ce828013a056bfdf33a6028a3d53
Will Urbowicz <wurbowicz@agilemile.com>
2022-11-18 11:41:09 -0500

    Section 10: Injecting Values from Properties File

--- spring-demo-annotations/src/main/java/com/wurbo/springdemo/SwimCoach.java
+++ spring-demo-annotations/src/main/java/com/wurbo/springdemo/SwimCoach.java
@@ -1,8 +1,15 @@
 package com.wurbo.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach{
 
     private FortuneService fortuneService;
+
+    @Value("${foo.email}")
+    private String email;
+    @Value("${foo.team}")
+    private String team;
 
     public SwimCoach(FortuneService theFortuneService){
         fortuneService = theFortuneService;
@@ -16,5 +23,13 @@ public class SwimCoach implements Coach{
     @Override
     public String getDailyFortune() {
         return fortuneService.getFortune();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTeam() {
+        return team;
     }
 }
