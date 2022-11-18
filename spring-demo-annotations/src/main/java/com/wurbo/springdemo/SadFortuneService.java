48d60c9dd787782e71e64a043f5ea0b8d476429a
Will Urbowicz <wurbowicz@agilemile.com>
2022-11-18 11:17:45 -0500

    Section 10: Defining Spring Beans with Java code (no xml)

---
+++ spring-demo-annotations/src/main/java/com/wurbo/springdemo/SadFortuneService.java
@@ -0,0 +1,8 @@
+package com.wurbo.springdemo;
+
+public class SadFortuneService implements FortuneService{
+    @Override
+    public String getFortune() {
+        return "Today is a sad day";
+    }
+}
--- spring-demo-annotations/src/main/java/com/wurbo/springdemo/SportConfig.java
+++ spring-demo-annotations/src/main/java/com/wurbo/springdemo/SportConfig.java
@@ -1,11 +1,23 @@
 package com.wurbo.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.wurbo.springdemo")
+//@ComponentScan("com.wurbo.springdemo")
 public class SportConfig {
 
+    // define bean for our sad fortune service
+    @Bean
+    public FortuneService sadFortuneService(){
+        return new SadFortuneService();
+    }
+
+    // define bean for our swim coach AND inject dependency
+    @Bean
+    public SwimCoach swimCoach(){
+        return new SwimCoach(sadFortuneService());
+    }
 
 }
