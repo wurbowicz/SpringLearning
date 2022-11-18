bec76964947345267d539a56d9637c112a13732b
Will Urbowicz <wurbowicz@agilemile.com>
2022-11-18 11:00:24 -0500

    Section 10: Spring Configuration with Java code (no xml)

---
+++ spring-demo-annotations/src/main/java/com/wurbo/springdemo/SportConfig.java
@@ -0,0 +1,11 @@
+package com.wurbo.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.wurbo.springdemo")
+public class SportConfig {
+
+
+}
